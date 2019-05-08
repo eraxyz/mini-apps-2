270cae9b4847eac95b7713afb08b6ff24c27530c
Luis Santana <lsantana30@protonmail.com>
2019-05-08 10:29:25 -0700

    Created code to add unqiue ids to all json event objects.

---
+++ challenge_1/data/jsonCleaner.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
+var json = JSON.parse(fs.readFileSync(path.join(__dirname, "./db.json")));
+
+dbOutput = fs.createWriteStream(path.join(__dirname, "./cleanedDB.json"));
+
+dbOutput.write('{"events": [');
+
+for (let i = 0; i < json.events.length; i++) {
+  Object.assign(json.events[i], { id: i + 1 });
+  if (i === json.events.length - 1) {
+    dbOutput.write(`\n${JSON.stringify(json.events[i])} `);
+  } else {
+    dbOutput.write(`\n${JSON.stringify(json.events[i])}, `);
+  }
+}
+
+dbOutput.write("]}");
