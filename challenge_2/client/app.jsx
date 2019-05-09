735e6f0539284bd7e68d8408d198a063a35e0e2c
Luis Santana <lsantana30@protonmail.com>
2019-05-08 18:52:31 -0700

    Added price index graph. Finished minimum requirements.

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,12 +1,43 @@
 import React from "react";
+import PriceIndexGraph from "./Components/PriceIndexGraph.jsx";
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      priceIndex: {}
+    };
   }
 
-  render() {}
+  componentDidMount() {
+    fetch("https://api.coindesk.com/v1/bpi/historical/close.json")
+      .then(response => response.json())
+      .then(coindeskObj => this.formatPriceIndex(coindeskObj.bpi, "Bitcoin"))
+      .then(priceIndex =>
+        this.setState({
+          priceIndex
+        })
+      )
+      .catch(err => console.error(err));
+  }
+
+  //format for Line Chart
+  formatPriceIndex(priceObj, cryptocurrencyName) {
+    let priceData = [];
+    for (let prop in priceObj) {
+      priceData.push(priceObj[prop]);
+    }
+    return {
+      labels: Object.keys(priceObj),
+      datasets: [{ label: cryptocurrencyName, data: priceData }]
+    };
+  }
+
+  render() {
+    const { priceIndex } = this.state;
+
+    return <PriceIndexGraph prices={priceIndex} />;
+  }
 }
 
 export default App;
