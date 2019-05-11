a7f013cbd83ab9234336fb577bd35494f5fb823f
Luis Santana <lsantana30@protonmail.com>
2019-05-10 18:17:40 -0700

    Changed html file location

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const db = require("./db/index");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use(express.static("client/dist"));
+app.use(bodyParser.json());
+
+app.get("/scoreboard", (req, res) => {
+  db.getScores((err, scores) => {
+    if (err) res.sendStatus(500);
+    else res.send(scores);
+  });
+});
+
+app.post("/addScore", (req, res) => {
+  db.addToScoreboard(req.body, err => {
+    console.log(err);
+  });
+  res.end();
+});
 
 app.listen(port, () => `Listening on port ${port}`);
